b0d36817d41a8a866d099d4af16c014fe26ac9c5
Simurgh101(Scotth) <suthiwatpraesi@hotmail.com>
2020-12-21 15:40:42 +1100

    fix something

--- src/apis/CoinGecko.jsx
+++ src/apis/CoinGecko.jsx
@@ -1,6 +1,5 @@
 import axios from 'axios';
-import React from 'react';
 
 export default axios.get(
-  'https://api.coingecko.com/api/v3/coins/markets?vs_currency=aud&order=market_cap_desc&per_page=250d&page=1&sparkline=false'
+    'https://api.coingecko.com/api/v3/coins/markets?vs_currency=aud&order=market_cap_desc&per_page=250d&page=1&sparkline=false'
 );
